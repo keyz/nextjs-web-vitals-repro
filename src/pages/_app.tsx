7fe8f2bdf72224d836d1dc1de3cd5105a0eba2ef
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-03-24 19:52:21 -0700

    repro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from "next/app";
+import { useReportWebVitals } from "next/web-vitals";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useReportWebVitals((metric) => {
+    console.log("1/ From `useReportWebVitals` hook:", metric);
+  });
+
   return <Component {...pageProps} />;
+}
+
+export function reportWebVitals(metric: any) {
+  console.log("2/ From `reportWebVitals` export:", metric);
 }
